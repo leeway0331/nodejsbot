96090921416fbdb10614c35f26245b9fe474add8
leeway0331 <leeway0331@gmail.com>
2020-08-12 17:07:44 +0900

    add clear command

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ client.on('message', (message) => {
 });
 
 function checkPermission(message) {
-  if(!message.member.hasBotPermission("MANAGE_MESSAGES")) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
     message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
     return true;
   } else {
