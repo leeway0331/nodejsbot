36f16e8cc7c27108d749b6c94bcd5fbc8543b6db
leeway0331 <leeway0331@gmail.com>
2020-08-29 19:20:44 +0900

    add kurl

--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzM3Mjg0MDk0ODQ0NzMxNDM0.Xx7HUg.NqYIftXP0N_94gjwXpfx8nuNVHw';
+const token = 'NzM3Mjg0MDk0ODQ0NzMxNDM0.Xx7HUg.FIvgOZwzt87ledBTfyWmIF9aYRA';
 
 client.on('ready', () => {
   console.log('켰다.');
